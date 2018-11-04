0dbbf01dc1161f30f9bc2fe686263a6bbbbb1d11
Aishwary Gupta <guptashark@gmail.com>
2018-11-04 16:45:16 -0500

    feat: create a wrapper function - error handling
    This wrapper fn prints the error to the stderr
    and also calls a longjmp so that the error
    is marked in the counters.

--- chapter_03/main.c
+++ chapter_03/main.c
@@ -13,23 +13,38 @@ int main(void) {
 	/* Errors is volatile since its value may be 
 	 * changed without a thread of execution 
 	 * directly changing it. For instance, 
-	 *
+	 * 
 	 * errors = 0
 	 * jump! 
 	 * errors = 1
 	 * jump back! 
-	 * 
+	 * execution assumes errors is 0 -> bad! 
+	 * volatile forces reload on every occassion: 
+	 * 	-> telling compiler that the value 
+	 * 	may change without this thread of 
+	 * 	executions knowledge
+	 */
 	volatile int errors = 0;
 
 	/* Where we store the arithmetic expression */
 	char buf[BUFSIZ];
 
-	
+
+	/* an error counter. setjmp returns zero 
+	 * initially. However, when longjmp 
+	 * returns, it returns to this location, 
+	 * and then setjmp returns whatever is 
+	 * in the longjmp arg. So, right now
+	 * there are zero errors, but if longjmp
+	 * finds one, it'll return as such, and 
+	 * we'll then add that to the errs counter, 
+	 */	
 	if(setjmp(onError)) {
 		errors++;
 	}
 
-	whille(fgets(buf, BUFSIZ, stdin)) {
+
+	while(fgets(buf, BUFSIZ, stdin)) {
 		if(scan(buf)) {
 			void * e = sum();
 			if(token) {
@@ -43,3 +58,20 @@ int main(void) {
 
 	return errors > 0;
 }
+
+/* takes in a format string with varargs, 
+ * initialize ptr to varargs
+ * use printf but to stderr, pass along varargs
+ * long jump with an error for errors counter
+ *
+ */
+void error(const char * fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+       	putc('\n', stderr);
+	va_end(ap);
+	longjmp(onError, 1);
+}
+
+
