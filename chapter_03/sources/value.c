bae371c6d11ebc47ca7aa1254b25d9188369df3a
Aishwary Gupta <guptashark@gmail.com>
2018-11-05 17:21:03 -0500

    Comments to fully understand the exec call
    It is fully understood how the parse tree,
    once built suitably is able to compute the
    value of the entered expression. What remains
    to be seen is how the parse tree is constructed,
    as in how it is decided which nodes go where.

--- chapter_03/sources/value.c
+++ chapter_03/sources/value.c
@@ -31,16 +31,75 @@ void * new (const void * type, ...)
 	return result;
 }
 
-static double exec (const void * tree)
+/* This is the fn that is called to 
+ * evaluate each node in the tree. 
+ *
+ * It is interesting to note that 
+ * exec is an internal function, 
+ * the main fn doesn't call exec
+ * to evaluate the tree... 
+ *
+ *  Why? 
+ *
+ *  Ans: The main fn can call the 
+ *  process fn. The difference is 
+ *  that process doesn't return 
+ *  anything - it prints directly 
+ *  to stdout, whereas exec does 
+ *  return things. We could print 
+ *  things in the main loop, but 
+ *  we choose to take away the 
+ *  burden of this and do it 
+ *  internally. It makes the main
+ *  loop less complex. 
+ */
+static double 
+exec (const void * tree)
 {
-	assert(tree && * (struct Type **) tree
-		&& (* (struct Type **) tree) -> exec);
+	/* ensure tree is not NULL, 
+	 * ensure that what tree points to 
+	 * is not NULL (basic check against 
+	 * an object that has no installed 
+	 * type)
+	 * ensure that there is a pointer 
+	 * where we expect exec to be. 
+	 *
+	 * Extremely primitive checking, 
+	 * since these random addresses 
+	 * could have garbage in them
+	 * and then it passes our asserts. 
+	 * There's nothing in place to 
+	 * check if the type that tree 
+	 * points to is a valid type. 
+	 *
+	 * TODO make a type checker 
+	 * by checking against a list of
+	 * the valid types that Type can 
+	 * be. 
+	 */
+	assert(tree)
+	assert(* (struct Type **) tree)
+	assert((* (struct Type **) tree) -> exec);
 
-	return (* (struct Type **) tree) -> exec(tree);
+	/* The dynamic function call. The type 
+	 * decides how the node will be evaluated, 
+	 * based on the fn inistalled in the 
+	 * type's exec field. 
+	 */
+	struct Type * t = *(struct Type **)tree;
+	return t->exec(tree);
+	/* 
+	 * return (* (struct Type **) tree) -> exec(tree);
+	 */
 }
 
 void process (const void * tree)
 {
+	/* The %g is a format specifier that 
+	 * uses the shortest repr of either
+	 * %e or %f. %e is for exponential 
+	 * notation with little e: 3.23e+2
+	 */
 	printf("\t%g\n", exec(tree));
 }
 
@@ -151,7 +210,6 @@ static double doAdd (const void * tree)
 	 * identical. The only point was to
 	 * unpack what is happening here
 	 */
-
 
 	/* Fun note: There are three levels of 
 	 * brackets. The outermost are for the 
