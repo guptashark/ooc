dbcf3639447bb91a42cb3ef7dcf1abd8974d4718
Aishwary Gupta <guptashark@gmail.com>
2018-11-05 11:58:09 -0500

    Copy all the files from online
    Since the code is somewhat fragmented
    and already hard to understand, it made
    sense to download all of it to at least
    get a good starting point - and then go
    on to make changes and understand it.

--- chapter_03/recognizer.c
+++ chapter_03/recognizer.c
@@ -33,7 +33,44 @@
  * buffer is exhausted is scan(buf) called to 
  * compute the next stuff. 
  *
+ */
+
+/* Processor
+ * simple arithmetic is mostly easy: 
+ * 	compute as soon as possible, 
+ * 	and pass back the results. 
  *
+ * Complicated expressions not easy: 
+ * 	Need to build a tree that can be 
+ * 	processed in traversals. 
  *
- *  
+ * 	a basic structure can be: 
+ * 	struct Node {
+ * 		enum tokens token;
+ * 		struct Node * left;
+ * 		struct Node * right;
+ * 	}
+ *
+ * 	but this is inflexible. Say we 
+ * 	have the token "number", and now 
+ * 	we need to store a number. then 
+ * 	we need a field for it... which 
+ * 	doesn't seem like a problem...? 
+ *
+ * 	Space is wasted in the case of 
+ * 	unary operators. (where it's 
+ * 	an fn like 'abs', because now
+ * 	one of the left and right nodes
+ * 	will be null. 
+ *
+ * 	It's... probably best to copy 
+ * 	over the files and then 
+ * 	implement them further instead
+ * 	of doing this, because the 
+ * 	book serves as a complement to 
+ * 	the source files that you're 
+ * 	supposed to have access to. 
+ * 	I'm sure that there will be 
+ * 	enough exercises to extend
+ * 	an arithmetic expression calc. 
  */
